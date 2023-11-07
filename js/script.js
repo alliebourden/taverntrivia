1f84008b18cd6718f0ac0d967c240bac35252d2d
alliebourden <127435154+alliebourden@users.noreply.github.com>
2023-11-07 15:48:00 -0330

    added start btn click, activeInfo info box

--- js/script.js
+++ js/script.js
@@ -2,3 +2,8 @@ const start_btn = document.querySelector(".start-btn button");
 const info_box = document.querySelector(".info-box");
 const exit_btn = info_box.querySelector(".buttons .exit-btn");
 const continue_btn = document.querySelector(".buttons .test-btn");
+
+// Start Button Click
+start_btn.onclick = () => {
+    info_box.classList.add("activeInfo");
+}
