3a78f28c4da80b2d020417baa6aa7e90ee32c02f
alliebourden <127435154+alliebourden@users.noreply.github.com>
2023-11-09 12:18:10 -0330

    question 2

--- questions.js
+++ questions.js
@@ -2,13 +2,24 @@
 let questions = [
     {
         numb: 1,
-        questions: "Which of the following is not a type of damage in D&D 5E?",
+        questions: "Which of the following is not a type of damage?",
         answer: "Sound",
         options: [
             "Slashing",
             "Necrotic",
             "Force",
             "Sound",
+        ]
+    },
+    {
+        numb: 2,
+        questions: "What is the name of the magical, sentient bag often used to carry a wide variety of items?",
+        answer: "Bag of Holding",
+        options: [
+            "Bag of Holding",
+            "Pouch of Infinite Storage",
+            "Sack of Secrets",
+            "Coinpurse of Wealth",
         ]
     },
 ];
